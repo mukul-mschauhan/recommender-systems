4efaf1887ad00a1b12dd91c5e4e7b5737f52981d
Mukul <mukul.mschauhan@gmail.com>
2024-09-26 14:12:49 +0530

    Final-Stage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # recommender-systems
 Building a Movie Recommender System Using Generative AI Models...
+
+### Create Virtual Environment
+command: ``python -m venv .venv``
+Mac: ``virtualenv.venv``
+
+### Activating the Virtual Environment 
+Mac/Windows: ``source .venv/Scripts/activate`` or
+Windows ``.venv/Scripts/activate``  
---
+++ app.py
@@ -0,0 +1,38 @@
+# Build a Recommender Systems...
+import streamlit as st
+from langchain import PromptTemplate, LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+import google.generativeai as genai
+
+
+# Import the Local Environment
+from dotenv import load_dotenv
+import os
+load_dotenv()
+genai.configure(api_key = os.getenv("GOOGLE-API-KEY"))
+
+
+# Design the Page... streamlit run app.py
+headers = {"authorization":st.secrets["GOOGLE-API-KEY"],
+           "content-type":"application/json"}
+
+st.title("🆒Movie Recommender Systems...")
+user_input = st.text_input("Enter the Movie title, genre or keyword")
+
+# Prompt Template
+demo_template = '''Based on {user_input} provide Movie Recommendations"'''
+template = PromptTemplate(input_variables=["user_input"], 
+                          template=demo_template)
+
+# Google Gemini Model..
+llm = ChatGoogleGenerativeAI(model = "gemini-pro", 
+                             api_key="GOOGLE-API-KEY")
+
+if user_input:
+    prompt = template.format(user_input = user_input)
+    recommendations = llm.predict(text=prompt)
+    st.write(f"Recommendations for you:\n {recommendations}")
+else:
+    st.write(" ")
+
+
